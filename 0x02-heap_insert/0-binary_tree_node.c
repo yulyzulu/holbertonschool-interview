e94c422637f3a27bf70263ea29cfdd9ca7ed478c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-12-22 21:29:50 +0000

    Added exercise to add new node in a binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+*binary_tree_node - Funciton that creates a new binary tree node
+*@parent: Pointer to the parent node of the node to create
+*@value: Value of the new node
+*Return: Pointer to the new node or null o failure
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+*heap_insert - Function that inserts a value into a Max Binary Heap
+*@root: Double pointer to the root node of the Heap
+*@value:  Value store in the node to be inserted
+*Return: Pointer to the inserted node, or NULL on failure
+*/
+heap_t *heap_insert(heap_t **root, int value)
+{
+}
