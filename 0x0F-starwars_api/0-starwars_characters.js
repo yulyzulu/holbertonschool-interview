a3f9a0ba1bef04e72fe555989952522d237bd851
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-08 20:52:13 +0000

    Starwars api

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
 
-request(url, function(err, res, body) {
+request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   }
@@ -13,12 +12,12 @@ request(url, function(err, res, body) {
   const character = data.characters;
   for (const i in character) {
     const url2 = character[i];
-    request(url2, function(err, res, body) {
+    request(url2, function (err, res, body) {
       if (err) {
         console.log(err);
       }
       const dataChar = JSON.parse(body);
       console.log(dataChar.name);
-    })
+    });
   }
 });
