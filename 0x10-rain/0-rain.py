2e4c0a2295bfa1fcc7583b898ae0922973ea2a3d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-14 13:51:26 +0000

    Rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+
 def rain(walls):
-    if len(walls) == 0:
+    wall_len = len(walls)
+    if wall_len == 0:
         return 0
-    new_list = []
-    for i in range(len(walls)):
-        if i > 0 and i < len(walls) - 2:
-            new_list.append(walls[i])
-    suma = sum(new_list)
-    return suma
+    count = 0
+    for i in range(1, wall_len - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        right = walls[i]
+        for j in range(i + 1, wall_len):
+           right = max(right, walls[j])
+        count = count + (min(left, right) - walls[i])
+    return count
