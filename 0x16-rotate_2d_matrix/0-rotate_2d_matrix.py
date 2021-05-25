4c2d95342865739593d27a45a1d3967e09e99382
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-25 13:58:41 +0000

    Rotate 2D matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotate 2d Matrix """
+    N = len(matrix)
+
+    for i in range(N):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+    for i in range(N):
+        for j in range(N // 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][N - j - 1]
+            matrix[i][N - j - 1] = temp
